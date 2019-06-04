417b6cb620c71bbfb7a6d6a90c23bf255202ad5b
Russ Abbott <Russ.Abbott@gmail.com>
2019-06-04 12:09:43 -0700

    PEP 8 clean-ups.

--- pylog/examples/puzzles.py
+++ pylog/examples/puzzles.py
@@ -1,6 +1,5 @@
 from typing import Callable, Type
 
-from logic_variables import Ground, Structure, Term, Var
 from sequence_options.super_sequence import SuperSequence
 
 
@@ -16,40 +15,6 @@ class SimpleCounter:
     return self
 
 
-# class StructureItem(Structure):
-#   """
-#   A utility class for building Structure-based items.
-#   """
-#
-#   def __init__(self, args, first_arg_as_str_functor=False):
-#     self.first_arg_as_str_functor = first_arg_as_str_functor
-#     super().__init__(args)
-#
-#   def __str__(self):
-#     """
-#     A default way to print out a structure. Use the first arg as the term name.
-#     """
-#     allArgs = all(isinstance(arg.trail_end(), Var) for arg in self.args)
-#     if allArgs:
-#       # If all the args are uninstantiated, print a simple underscore.
-#       return '_'
-#     else:
-#       args_str = ', '.join(map(str, self.args[1:]))
-#       result = f'{self.args[0]}({args_str})'
-#       return result
-#
-#   @staticmethod
-#   def make_property(prop):
-#     """
-#       Applied to each argument in a term.
-#       Applies Ground to those that are not already Terms.
-#       If the property is None, create a Var for it.
-#     """
-#     return Var() if prop is None else \
-#                     prop if isinstance(prop, Term) else \
-#                     Ground(prop)
-#
-#
 def run_puzzle(problem: Callable, ListType: Type, Answer_List: SuperSequence, additional_answer: Callable = None):
   """ Runs the problem and displays the answer. Takes and displays timing information. """
 
@@ -59,8 +24,8 @@ def run_puzzle(problem: Callable, ListType: Type, Answer_List: SuperSequence, ad
   for _ in problem(Answer_List):
     end1 = timer( )
     print('\nSolution: ')
-    for (index, student) in enumerate(Answer_List):
-      print(f'\t{index + 1}. {student}')
+    for (index, item) in enumerate(Answer_List):
+      print(f'\t{index + 1}. {item}')
     if additional_answer:
       additional_answer(Answer_List)
     inp = input('\nMore? (y, or n)? > ').lower( )
