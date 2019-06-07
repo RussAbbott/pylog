055dfba3d023f31290aa2f4a25a955f75f2ba816
Russ Abbott <Russ.Abbott@gmail.com>
2019-06-06 18:15:43 -0700

    Add files via upload

--- pylog/examples/yield_vs_return.py
+++ pylog/examples/yield_vs_return.py
@@ -1,13 +1,16 @@
 
 
+# In this example, the results are sent backward through implicit yield's rather than returns. That is,
+# the nested for-loops send their results to the previous ones essentially through yield statements.
 print('1. ', end = '')
 xyz_s = [ x+y+z for x in 'ab' for y in ("de" if x == 'a' else 'fg') for z in ('hi' if y == 'd' else 'jk')]
 for xyz in xyz_s:
   print(xyz, end = ', ')
 print()
 
-# Can we do this with nested function calls?
-# Yes, but the end results are generated at the bottom rather than returned to the top.
+# Can we do this with nested function calls? This is the same thing, but but done without yields.
+# The results are the same, but the only way to get them is at the bottom of nested calls. Note that
+# there are no return statements. Things are always passed forward.
 
 print('2. ', end='')
 
@@ -45,9 +48,11 @@ def bottom3(x, y):
 print('3.', top3())
 
 
-# But try it with yield instead of return. This produces the same result as 1. The results are
-# returned to and printed at the top. This works because the downward calls are done in for-loops,
-# and the results are returned through yields, which doesn't terminate the for-loops
+# But when done with yield instead of return, we get the same result as 1. The results are
+# returned to and printed at the top. This works because the downward calls are done in for-loops
+# rather than direct function calls, and the results are returned through yields rather than return
+# statements. A yield does n0t terminate the for-loop within which it is embedded -- just like the
+# first example.
 
 def top4( ):
   for x in 'ab':
