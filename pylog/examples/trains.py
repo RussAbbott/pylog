d9a48df59f2ec8fa5079bd8bdd558ab7f5e96b59
Russ Abbott <Russ.Abbott@gmail.com>
2019-06-08 21:21:49 -0700

    Clean-up.

--- pylog/examples/trains.py
+++ pylog/examples/trains.py
@@ -2,7 +2,7 @@ from functools import reduce
 from typing import Iterator, List, Tuple, Union
 
 from control_structures import forall
-from logic_variables import PyValue, Term, unify, unify_pairs
+from logic_variables import PyValue, Term, unify
 
 
 lines = {
@@ -21,13 +21,18 @@ def best_route(Start: PyValue, End: PyValue):
   The best route uses the fewest lines, and of routes using the same number of lines, the fewest total Dist values.
   """
   # Look for routes that use the fewest lines.
-  for i in range(len(lines)):
+  for i in range(1, len(lines)+1):
+    # i is the number of lines used.
     # The middle sequence is the intermediate lines and stations.
-    intermediate = (PyValue() for _ in range(2*i+1))
+    # One line involves one intermediate element, the line used.
+    # Two lines involves 3 intermediate elements, the two lines used and the transfer station.
+    # Etc.
+    # The "+1" above allows i to take on the value len(lines), in which case we are using all the lines.
+    intermediate = (PyValue() for _ in range(2*i-1))
     legs = [Start, *intermediate, End]
     # If route(*legs) succeeds, route will instantiate legs to
     #         [Station, (Line, int), Station, (Line, int), ... , Station]
-    # Once legs is instantiated, must take the py_values so that the collection
+    # Once legs is instantiated, must extract the py_values so that the collection
     # of routes remains instantiated after the list comprehension terminates.
     route_options = [ [elt.get_py_value() for elt in legs] for _ in route(*legs) ]
     # Once we find an i so that there is at least one route from Start to End using i lines,
@@ -51,7 +56,6 @@ def connected(S1: PyValue, Line_Dist: PyValue, S2: PyValue):
   Line_Dist will be unified with (line, count_of_stations)
   """
   # print(f'-> connected({S1}, {Line_Dist}, {S2})?')
-  # if S1 != S2:
   Line = PyValue()
   for _ in forall([lambda: has_station(Line, S1),
                    lambda: has_station(Line, S2)]):
@@ -68,11 +72,12 @@ def connected(S1: PyValue, Line_Dist: PyValue, S2: PyValue):
 def has_station(L: PyValue, S: PyValue):
   # print(f'-> has_station({L}, {S})?')
   for line in lines:
-    for station in lines[line]:
-      for _ in unify_pairs([(L, line),
-                            (S, station)]):
-        # print(f'<- has_station({L}, {S})')
-        yield
+    # If L is instantiated, find out whether it is line before looking at all the stations.
+    for _ in unify(L, line):
+      for station in lines[line]:
+        for _ in unify(S, station):
+          # print(f'<- has_station({L}, {S})')
+          yield
   # print(f'XX has_station({L}, {S})')
 
 
@@ -81,14 +86,15 @@ def route(*legs: List[Term]):
   Can we get from the first station to the last? If so, which lines and
   which intermediate stations should we use?
   :param legs: a sequence of: station, line, station, line, ... station.
-         All but the first and last will be PyValues.
   :return: Find a route and instantiate the intermediate PyValues.
   """
   # print(f'-> route({[str(leg) for leg in legs]})?')
-  if len(legs) == 1:  # Have arrived at our destination
-    # succeed
+  if len(legs) == 1:
+    # We have arrived at our destination. succeed
     # print(f'<- route({[str(leg) for leg in legs]}?')
     yield
+
+  # len(legs) should never be 0 or 2.
   elif len(legs) >= 3:
     # Take the next leg.
     for _ in connected(*legs[:3]):
@@ -126,7 +132,7 @@ def sum_distances(legs: List[Union[str, Tuple[str, int]]]) -> Tuple[List[str], i
 
 if __name__ == '__main__':
 
-  def s(nbr):
+  def add_plural_s(nbr):
     return '' if nbr == 1 else 's'
 
   def print_route(stations_and_lines: List[str], stations_passed: int):
@@ -135,9 +141,9 @@ if __name__ == '__main__':
       (station, line, next_station) = stations_and_lines[2*i:2*i+3]
       print(f'\tFrom {station} take the {line} line to {next_station}.')
 
-    transfer_stns_insert = f', including {nbr_lines -1} transfer station{s(nbr_lines -1)}' if nbr_lines > 1 else ''
-    print(f'This route uses {nbr_lines} line{s(nbr_lines)} and ', end='')
-    print(f'passes {stations_passed - 1} intermediate station{s(stations_passed - 1)}{transfer_stns_insert}.')
+    transfer_stns = f', including {nbr_lines -1} transfer station{add_plural_s(nbr_lines -1)}' if nbr_lines > 1 else ''
+    print(f'This route uses {nbr_lines} line{add_plural_s(nbr_lines)} and passes {stations_passed - 1} ', end='')
+    print(f'intermediate station{add_plural_s(stations_passed - 1)}{transfer_stns}.')
 
 
   for (s1, s2) in [("Takatsuki", "Yamashina"),  # Direct
@@ -162,6 +168,6 @@ if __name__ == '__main__':
     # In this case it's simply a basket in which the best route is conveyed.
     # Route = Var( )
     for Route in best_route(S1, S2):
-      print(f'\nA route from {S1} to {S2} that uses fewest lines ', end='')
-      print(f'and of those that passes fewest intermediate stations:')
+      print(f'\nA route from {S1} to {S2} that uses fewest lines, ', end='')
+      print(f'and of those the one that passes the fewest intermediate stations:')
       print_route( *Route )
